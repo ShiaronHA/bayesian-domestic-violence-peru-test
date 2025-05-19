a55f62323bd65bf8e93f60ca5c3514dfbb826c8b
Rutbel Huapaya <rutbel.huapaya.contractor@bbva.com>
2025-05-20 00:27:59 +0200

    Config variables ordinales y GES

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Modularización y orden del código
 import pandas as pd
 import pickle
 import os
@@ -9,27 +10,97 @@ from pgmpy.inference import BeliefPropagation
 from pgmpy.readwrite import BIFWriter
 from pgmpy.estimators import BayesianEstimator
 from pgmpy.metrics import structure_score
+from pgmpy.estimators import GES
 
-
+# --- Preprocesamiento de datos ---
 def preprocess_data(filepath):
     df = pd.read_csv(filepath, delimiter=',')
     print("Forma inicial del DataFrame:", df.shape)
     df = df.dropna()
-    df = df.astype('category')
+    # Categorización explícita de variables ordinales
+    df.NIVEL_EDUCATIVO_VICTIMA = pd.Categorical(df.NIVEL_EDUCATIVO_VICTIMA,
+        categories=[
+            'SIN NIVEL/INICIAL/BASICA ESPECIAL',
+            'PRIMARIA INCOMPLETA',
+            'PRIMARIA COMPLETA',
+            'SECUNDARIA INCOMPLETA',
+            'SECUNDARIA COMPLETA',
+            'SUPERIOR TECNICO/UNIVERSITARIO INCOMPLETO',
+            'SUPERIOR TECNICO/UNIVERSITARIO COMPLETO',
+            'MAESTRIA / DOCTORADO'],
+        ordered=True)
+    df.NIVEL_EDUCATIVO_AGRESOR = pd.Categorical(df.NIVEL_EDUCATIVO_AGRESOR,
+        categories=[
+            'SIN NIVEL/INICIAL/BASICA ESPECIAL',
+            'PRIMARIA INCOMPLETA',
+            'PRIMARIA COMPLETA',
+            'SECUNDARIA INCOMPLETA',
+            'SECUNDARIA COMPLETA',
+            'SUPERIOR TECNICO/UNIVERSITARIO INCOMPLETO',
+            'SUPERIOR TECNICO/UNIVERSITARIO COMPLETO',
+            'MAESTRIA / DOCTORADO'],
+        ordered=True)
+    df.EDAD_VICTIMA = pd.Categorical(
+        df.EDAD_VICTIMA,
+        categories=[
+            'PRIMERA INFANCIA',
+            'INFANCIA',
+            'ADOLESCENCIA',
+            'JOVEN',
+            'ADULTO JOVEN',
+            'ADULTO',
+            'ADULTO MAYOR'
+        ],
+        ordered=True
+    )
+    df.EDAD_AGRESOR = pd.Categorical(
+        df.EDAD_AGRESOR,
+        categories=[
+            'PRIMERA INFANCIA',
+            'INFANCIA',
+            'ADOLESCENCIA',
+            'JOVEN',
+            'ADULTO JOVEN',
+            'ADULTO',
+            'ADULTO MAYOR'
+        ],
+        ordered=True
+    )
+    df.FRECUENCIA_AGREDE = pd.Categorical(
+        df.FRECUENCIA_AGREDE,
+        categories=['MENSUAL', 'QUINCENAL', 'SEMANAL', 'INTERMITENTE', 'DIARIO'],
+        ordered=True
+    )
+    df.NIVEL_DE_RIESGO_VICTIMA = pd.Categorical(
+        df.NIVEL_DE_RIESGO_VICTIMA,
+        categories=['LEVE', 'MODERADO', 'SEVERO'],
+        ordered=True
+    )
+    # Variables nominales (no ordenadas)
+    nominal_cols = [
+        'CONDICION', 'ETNIA_VICTIMA', 'LENGUA_MATERNA_VICTIMA', 'AREA_RESIDENCIA_DOMICILIO',
+        'ESTADO_CIVIL_VICTIMA', 'TRABAJA_VICTIMA', 'VINCULO_AGRESOR_VICTIMA',
+        'AGRESOR_VIVE_CASA_VICTIMA', 'TRATAMIENTO_VICTIMA', 'SEXO_AGRESOR', 'ESTUDIA',
+        'ESTADO_AGRESOR_U_A', 'ESTADO_AGRESOR_G', 'ESTADO_VICTIMA_U_A', 'ESTADO_VICTIMA_G',
+        'REDES_FAM_SOC', 'SEGURO_VICTIMA', 'VINCULO_AFECTIVO', 'VIOLENCIA_ECONOMICA',
+        'VIOLENCIA_PSICOLOGICA', 'VIOLENCIA_SEXUAL', 'VIOLENCIA_FISICA', 'HIJOS_VIVIENTES'
+    ]
+    for col in nominal_cols:
+        df[col] = pd.Categorical(df[col], ordered=False)
     df_encoded = df.apply(lambda col: col.cat.codes if col.dtype.name == 'category' else col)
     category_mappings = {
-    	col: dict(enumerate(df[col].cat.categories))
+        col: dict(enumerate(df[col].cat.categories))
         for col in df.select_dtypes(['category']).columns
     }
     print(df_encoded)
     return df_encoded, category_mappings
 
+# --- Aprendizaje de estructura ---
 def learn_structure(df, algorithm='hill_climb', scoring_method=None, output_path=None):
-
+    df.info()
     if algorithm == 'hill_climb':
         print(f"\nAprendiendo con Hill Climbing usando {scoring_method}...")
         est = HillClimbSearch(df)
-
         if scoring_method == 'bic':
             model = est.estimate(scoring_method=BicScore(df), max_iter=5000, max_indegree=5)
         elif scoring_method == 'bdeu':
@@ -38,18 +109,21 @@ def learn_structure(df, algorithm='hill_climb', scoring_method=None, output_path
             model = est.estimate(scoring_method='k2score', max_indegree=5, max_iter=int(1e4))
         else:
             raise ValueError("Scoring method no soportado para Hill Climbing.")
-
         bn_model = BayesianNetwork(model.edges())
-
+    elif algorithm == 'GES':
+        print(f"\nAprendiendo con GES...")
+        est = GES(df)
+        model = est.estimate(scoring_method='bic-cg')
+        bn_model = BayesianNetwork(model.edges())
     elif algorithm == 'pc':
         print(f"\nAprendiendo con PC...")
         est = PC(df)
-        model = est.estimate(ci_test='chi_square', variant="stable", max_cond_vars=4, return_type='dag')
+        model = est.estimate(ci_test='pillai')
+        #model = est.estimate(ci_test='chi_square', variant="stable", max_cond_vars=4, return_type='dag')
         bn_model = BayesianNetwork(model.edges())
-
     elif algorithm == 'mmhc':
         print("\nAprendiendo con MMHC (Max-Min Hill Climbing)...")
-        columns_to_exclude = ["NIVEL_EDUCATIVO_AGRESOR","NIVEL_EDUCATIVO_VICTIMA","EDAD_VICTIMA", "CONDICION", "ETNIA_VICTIMA"]
+        columns_to_exclude = ["NIVEL_EDUCATIVO_AGRESOR", "NIVEL_EDUCATIVO_VICTIMA", "EDAD_VICTIMA", "CONDICION", "ETNIA_VICTIMA"]
         mmhc_df = df.drop(columns=[col for col in columns_to_exclude if col in df.columns])
         mmhc = MmhcEstimator(mmhc_df)
         skeleton = mmhc.mmpc()
@@ -60,133 +134,105 @@ def learn_structure(df, algorithm='hill_climb', scoring_method=None, output_path
             white_list=skeleton.to_directed().edges(),
             scoring_method=BDeuScore(mmhc_df),
             max_indegree=3,
-	        max_iter=100 #1000
+            max_iter=100
         )
         bn_model = BayesianNetwork(model.edges())
     else:
         raise ValueError("Algoritmo no soportado.")
-
-    
     print("Estructura aprendida:", bn_model.edges())
-    
     return bn_model
 
-
+# --- Main para experimentos completos ---
 def main():
     df, dict = preprocess_data('data/df_processed.csv')
-
+    # algorithms_to_experiment = [
+    #     ('hill_climb', 'bic'),
+    #     ('hill_climb', 'k2'),
+    #     ('hill_climb', 'bdeu'),
+    #     ('pc', 'bdeu'),
+    #     ('mmhc', 'bic'),
+    #     ('mmhc', 'bdeu')
+    # ]
     algorithms_to_experiment = [
         ('hill_climb', 'bic'),
-        ('hill_climb', 'k2'),
         ('hill_climb', 'bdeu'),
-	    ('mmhc','bdeu')
-        #('pc', None)
+        ('pc', 'pillai'),
+        ('GES', 'bic-cg')
     ]
-    	
-
-    sample_sizes = [200,1000]
+    sample_sizes = [200, 1000, 30000]
     results = []
     trained_models = {}
-
     for sample_size in sample_sizes:
         sample_data = df.sample(n=sample_size, random_state=42).reset_index(drop=True)
         print("Forma de muestra del DataFrame:", sample_data.shape)
-	# Aprendizaje de la estructura
-        for algorithm,score_method  in algorithms_to_experiment:
+        for algorithm, score_method in algorithms_to_experiment:
             print(f"\nAprendiendo estructura con {algorithm} with sample size = {sample_size}...")
             start_time = time.time()
             model = learn_structure(df=sample_data, algorithm=algorithm, scoring_method=score_method,
-                                            output_path=f'./uploads/model_structure_29_{algorithm}_{score_method if algorithm =='hill_climb' else 'BDeu'}_{sample_size}.pkl')
-
-            # Asegurarse de que las columnas del DataFrame coincidan con las variables del modelo
+                output_path=f'./uploads/model_structure_29_{algorithm}_{score_method if algorithm == "hill_climb" else "BDeu"}_{sample_size}.pkl')
             model_variables = set(var for edge in model.edges() for var in edge)
-            # Convertir el conjunto de variables del modelo a una lista antes de usarlo como indexador
             df_filtered = df[list(model_variables)]
-
             score = structure_score(model, df_filtered, scoring_method="bdeu")
             print("Calidad de red BDeue:", score)
             elapsed_time = time.time() - start_time
-            key = f"{algorithm}_{score_method if algorithm =='hill_climb' else 'bdeu'}_{sample_size}"
+            key = f"{algorithm}_{score_method if algorithm == 'hill_climb' else 'bdeu'}_{sample_size}"
             trained_models[key] = model
             results.append({'Model': model,
                             'BDeu_Score': score,
-                            'Score_method': score_method if algorithm =='hill_climb' else 'bdeu',
+                            'Score_method': score_method if algorithm == 'hill_climb' else 'bdeu',
                             'Algorithm': algorithm,
                             'Sample_Size': sample_size,
                             'Training_Time_Seconds': elapsed_time,
                             'Number_of_Edges': len(model),
-			    'Number_of_df_variables':len(df.columns)
+                            'Number_of_df_variables': len(df.columns)
                             })
-
     results_df = pd.DataFrame(results)
-    print("\nTabla comparativa de resultados:") 
+    print("\nTabla comparativa de resultados:")
     print(results_df.to_string(index=False))
-
-    # Guardar los resultados en un archivo CSV
     comparison_file_path = os.path.join('./uploads', 'resultados.csv')
     results_df.to_csv(comparison_file_path, index=False)
     print(f"Resultados guardados en: {comparison_file_path}")
-
-    # Guardar mejor modelo
     best_row = results_df.sort_values(by='BDeu_Score', ascending=False).iloc[0]
-    # Corregir la asignación de `best_model_key` para que coincida con las claves del diccionario `trained_models`
     best_model_key = f"{best_row['Algorithm']}_{best_row['Score_method']}_{int(best_row['Sample_Size'])}"
     best_score = best_row['BDeu_Score']
     best_model = trained_models[best_model_key]
-
     filename = f"./uploads/mejor_modelo_{best_model_key}_bic{best_score:.2f}_edges{len(best_model.edges())}.pkl"
-    # Guardar el modelo como archivo pickle
     with open(filename, 'wb') as f:
         pickle.dump(best_model, f)
-
     print(f"\nEl mejor modelo ha sido guardado en: {filename}")
-
-    #Evaluando y guardando el mejor modelo
-    model = best_model #Probando....
-
-    #BayesianEstimator
-    print("\nEstimando parámetros con BayesianEstimator...") 
-    estimator = BayesianEstimator(model, df)
-    model.fit(df, estimator=BayesianEstimator, prior_type='BDeu')
-
+    # Aprendizaje de parámetros
+    print("\nEstimando parámetros con BayesianEstimator...")
+    estimator = BayesianEstimator(best_model, df)
+    best_model.fit(df, estimator=BayesianEstimator, prior_type='BDeu')
     # Guardar el modelo en la carpeta uploads con el formato BIF
     model_file_path = os.path.join('./uploads', 'mi_modelo_red.bif')
-    writer = BIFWriter(model)
+    writer = BIFWriter(best_model)
     writer.write_bif(model_file_path)
-
     print(f"El modelo ha sido guardado en: {model_file_path}")
-    
     # Guardar los mapeos (dict)
     dict_file_path = os.path.join('./uploads', 'categorical_mappings.json')
     with open(dict_file_path, 'w', encoding='utf-8') as f:
         json.dump(dict, f, indent=4, ensure_ascii=False)
     print(f"Mapeos de categorías guardados en: {dict_file_path}")
-
-
-    #Inferencia exacta
+    # Inferencia exacta
     print("\nRealizando inferencia exacta ...")
-    
-    belief_propagation = BeliefPropagation(model)
+    belief_propagation = BeliefPropagation(best_model)
     print("Calibrate belief propagation ...")
     belief_propagation.calibrate()
     print("Realizando primera inferencia ...")
-    # Definir las evidencias
     evidence = {
         'VINCULO_AGRESOR_VICTIMA': 1,
         'ESTUDIA': 1,
         'NIVEL_EDUCATIVO_VICTIMA': 2,
         'AREA_RESIDENCIA_DOMICILIO': 1
     }
-
-    result = belief_propagation.map_query(variables=['NIVEL_DE_RIESGO_VICTIMA'],evidence=evidence)
+    result = belief_propagation.map_query(variables=['NIVEL_DE_RIESGO_VICTIMA'], evidence=evidence)
     print("Resultado de inferencia NIVEL_DE_RIESGO_VICTIMA:", result)
-    # Guardar resultados de inferencia
     result = {k: int(v) if hasattr(v, 'item') else v for k, v in result.items()}
     result_file_path = os.path.join('./uploads', 'inferencia_resultado.json')
     with open(result_file_path, 'w', encoding='utf-8') as f:
         json.dump(result, f, indent=4, ensure_ascii=False)
     print(f"Resultado de la inferencia guardado en: {result_file_path}")
-
 
 if __name__ == "__main__":
     main()
