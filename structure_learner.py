f15f3338b156ebda9d6bed7df6038ac87571c196
Shian <rhuapayaa@pucp.edu.pe>
2025-06-04 04:59:08 -0500

    dtype definitions.json

--- structure_learner.py
+++ structure_learner.py
@@ -252,10 +252,10 @@ def main():
         ('hill_climb', 'bic-d'), 
         #('hill_climb', 'k2'),
         ('hill_climb', 'bdeu'),
-        ('pc', 'pillai'),
-        ('pc', 'chi_square'),
-	    ('GES','bic-d'),
-        ('GES', 'bic-cg')
+        #('pc', 'pillai'),
+        #('pc', 'chi_square'),
+	#    ('GES','bic-d'),
+        #('GES', 'bic-cg')
         #('mmhc', 'bic'),
 	    #('mmhc', 'bdeu')
     ]
