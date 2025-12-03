560081f98014e1c00294aadd6cfe169cd95a4eb9
Rutbel Huapaya <rutbel.huapaya.contractor@bbva.com>
2025-12-03 16:43:20 +0100

    Adecuación para experimento con effect_size_threshold en LLM

--- expert_in_the_loop.py
+++ expert_in_the_loop.py
@@ -101,13 +101,13 @@ def main():
 
     llm_models = [ "gemini/gemini-2.0-flash", "gemini/gemini-pro"] #"gpt-4",
     results = []
-
+    effect_size_threshold = 0.0001  # 0.01
     for llm_model in llm_models:
         print(f"\nEntrenando con LLM: {llm_model}")
         start_time = time.time()
         try:
             dag = estimator.estimate(pval_threshold=0.05, 
-                                    effect_size_threshold=0.01, 
+                                    effect_size_threshold=effect_size_threshold, 
                                     variable_descriptions=descriptions,
                                     use_llm=True,
                                     llm_model=llm_model)
@@ -147,12 +147,13 @@ def main():
                 'Sample_Size': len(train_df_for_eil),
                 'Training_Time_Seconds': elapsed_time,
                 'Number_of_Edges': len(bn_model.edges()),
-                'Number_of_df_variables': len(train_df_for_eil.columns)
+                'Number_of_df_variables': len(train_df_for_eil.columns),
+                'Effect_Size_Threshold': effect_size_threshold
             })
             
             #Guardar el DAG aprendido individualmente
             safe_llm_name = llm_model.replace('/', '_').replace('-', '_')
-            filename = f"./models/dag_aprendido_with_llm_{safe_llm_name}_bicScore{score_bic:.2f}.pkl"
+            filename = f"./models/dag_aprendido_with_llm_{safe_llm_name}_bicScore{score_bic:.2f}_effect{effect_size_threshold}.pkl"
             with open(filename, 'wb') as f:
                 pickle.dump(bn_model, f)
             print(f"El DAG aprendido ({llm_model}) ha sido guardado en: {filename}")
@@ -168,7 +169,7 @@ def main():
         print("\nTabla comparativa de resultados (ExpertInLoop):")
         print(results_eil.to_string(index=False))
         
-        comparison_file_path = os.path.join('./results', 'resultados_expert_in_the_loop.csv')
+        comparison_file_path = os.path.join('./results', 'resultados_expert_in_the_loop_effect_size_0.0001.csv')
         results_eil.to_csv(comparison_file_path, index=False)
         print(f"Resultados guardados en: {comparison_file_path}")
     else:
